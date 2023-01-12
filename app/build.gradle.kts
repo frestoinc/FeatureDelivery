0ca25abdc03d28d18a53492ac529387ce56b203b
frestoinc <aliffridzuan88@gmail.com>
2023-01-12 12:32:07 +0800

    Add: install time introduction module

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
 
 android {
     namespace = "com.frestoinc.sample.featuredelivery"
+    dynamicFeatures += setOf(":installtime:introduction")
 }
 
 dependencies {
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -16,3 +16,4 @@ dependencyResolutionManagement {
 }
 rootProject.name = "FeatureDelivery"
 include(":app")
+include(":installtime:introduction")
